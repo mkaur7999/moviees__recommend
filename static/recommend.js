c194b04d10fd7f7524321f92943b22d1f2854596
mkaur7999 <mkaur77999@gmail.com>
2021-11-20 20:47:11 +0530

    update1

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '35f9e33237d9417fe721e958ff21909b';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '35f9e33237d9417fe721e958ff21909b';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
